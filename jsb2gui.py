8539a3c1993ef37e50fa2113bb2ff76f7d2ac71f
Maximiliano Churichi <mchurichi@gmail.com>
2012-02-24 13:52:53 -0300

    Added splitter and console to the ui. Some minor string format fixs

--- jsb2gui.py
+++ jsb2gui.py
@@ -23,7 +23,7 @@ class JSB2_GUI(QMainWindow):
 
     @pyqtSlot('bool')
     def on_action_Open_triggered(self, checked=None):
-        file_name = QFileDialog.getOpenFileName(self, 'Open file')
+        file_name = QFileDialog.getOpenFileName(self, u'Open file')
         if file_name:
             f = open(file_name, 'r')
             json_file = f.read()
@@ -38,26 +38,26 @@ class JSB2_GUI(QMainWindow):
             ]
             # Packages
             pkgs = QTreeWidgetItem(\
-                [u'pkgs', '[%i packages]' % len(jsb2['pkgs'])])
+                [u'pkgs', u'[%i packages]' % len(jsb2['pkgs'])])
             for pkg in jsb2['pkgs']:
                 child_pkg = QTreeWidgetItem([pkg['name']])
                 child_pkg.addChild(QTreeWidgetItem([u'name', pkg['name']]))
                 child_pkg.addChild(QTreeWidgetItem([u'file', pkg['file']]))
                 if 'isDebug' in pkg:
-                    idebug = str(pkg['isDebug'])
+                    idebug = unicode(pkg['isDebug'])
                     child_pkg.addChild(QTreeWidgetItem([u'isDebug', idebug]))
                 if 'includeDeps' in pkg:
-                    ideps = str(pkg['includeDeps'])
+                    ideps = unicode(pkg['includeDeps'])
                     child_pkg.addChild(QTreeWidgetItem([u'includeDeps', ideps]))
                 # Dependencies
                 if 'pkgDeps' in pkg:
-                    num_deps = '[%i dependencies]' % len(pkg['pkgDeps'])
-                    pkg_deps = QTreeWidgetItem(['pkgDeps', num_deps])
+                    num_deps = u'[%i dependencies]' % len(pkg['pkgDeps'])
+                    pkg_deps = QTreeWidgetItem([u'pkgDeps', num_deps])
                     for dep in pkg['pkgDeps']:
                         pkg_deps.addChild(QTreeWidgetItem([dep]))
                     child_pkg.addChild(pkg_deps)
                 # Files included
-                num_files = '[%i files]' % len(pkg['fileIncludes'])
+                num_files = u'[%i files]' % len(pkg['fileIncludes'])
                 fileIncludes = QTreeWidgetItem([u'fileIncludes', num_files])
                 for file in pkg['fileIncludes']:
                     child_f = QTreeWidgetItem([file['text']])
@@ -71,7 +71,7 @@ class JSB2_GUI(QMainWindow):
             items.append(pkgs)
 
             # Top level key Resources
-            num_res = '[%i resources]' % len(jsb2['resources'])
+            num_res = u'[%i resources]' % len(jsb2['resources'])
             resources = QTreeWidgetItem([u'resources', num_res])
             for res in jsb2['resources']:
                 child_r = QTreeWidgetItem([res['src']])
