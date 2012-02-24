537312919e7bfa5e720e80af6ac82b23445ebfcf
Maximiliano Churichi <mchurichi@gmail.com>
2012-02-24 19:49:38 -0300

    First test of tree editor

--- jsb2gui.py
+++ jsb2gui.py
@@ -5,11 +5,13 @@ import sys
 import json
 from PyQt4.QtCore import QObject, SIGNAL, pyqtSlot
 from PyQt4.QtGui import QApplication, QMainWindow, QWidget, QTreeWidgetItem, \
-    QFileDialog
+    QFileDialog, QFormLayout, QLineEdit, QLabel
+from PyQt4.QtCore import Qt
 from jsb2mainwindow import Ui_MainWindow
 
 
 class JSB2_GUI(QMainWindow):
+
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
@@ -18,12 +20,34 @@ class JSB2_GUI(QMainWindow):
             SIGNAL('itemSelectionChanged()'), self.selected)
 
     def selected(self):
-        pass
-#        print self.ui.treeJSB.selectedItems()[0].setText(1, 'lalalal')
+        selected = self.ui.treeJSB.selectedItems()[0]
+        if selected.data(0, Qt.UserRole).toString():
+            print selected.data(0, Qt.UserRole).toString()
+#        else: print type(selected.data(0, Qt.UserRole))
 
+        if not hasattr(self.ui, 'label22'):
+            self.ui.label22 = QLabel(self.ui.layoutWidget1)
+            self.ui.label22.setText(QApplication.translate("MainWindow", \
+                "TextLabel", None, QApplication.UnicodeUTF8))
+            self.ui.label22.setObjectName("label")
+            self.ui.formLayout.setWidget(2, QFormLayout.LabelRole, \
+                self.ui.label22)
+            self.ui.lineEdit22 = QLineEdit(self.ui.layoutWidget1)
+            self.ui.lineEdit22.setObjectName("lineEdit")
+            self.ui.formLayout.setWidget(2, QFormLayout.FieldRole, \
+                self.ui.lineEdit22)
+
+    def _new_tree_item(self, key, value=None, user_data=None):
+        item = QTreeWidgetItem([key, value])
+        if user_data:
+            item.setData(0, Qt.UserRole, user_data)
+        return item
+
+    #TODO: remove file_name argument
     @pyqtSlot('bool')
-    def on_action_Open_triggered(self, checked=None):
-        file_name = QFileDialog.getOpenFileName(self, u'Open file')
+    def on_action_Open_triggered(self, file_name=None):
+        if not file_name:
+            file_name = QFileDialog.getOpenFileName(self, u'Open file')
         if file_name:
             f = open(file_name, 'r')
             json_file = f.read()
@@ -32,9 +56,15 @@ class JSB2_GUI(QMainWindow):
 
             # Top leves keys
             items = [
-                QTreeWidgetItem([u'projectName', jsb2['projectName']]),
-                QTreeWidgetItem([u'deployDir', jsb2['deployDir']]),
-                QTreeWidgetItem([u'licenseText', jsb2['licenseText']])
+#                QTreeWidgetItem([u'projectName', jsb2['projectName']]),
+#                QTreeWidgetItem([u'deployDir', jsb2['deployDir']]),
+#                QTreeWidgetItem([u'licenseText', jsb2['licenseText']])
+                self._new_tree_item(u'projectName', jsb2['projectName'], \
+                    u'projectName'),
+                self._new_tree_item(u'deployDir', jsb2['deployDir'], \
+                    u'deployDir'),
+                self._new_tree_item(u'licenseText', jsb2['licenseText'], \
+                    u'licenseText')
             ]
             # Packages
             pkgs = QTreeWidgetItem(\
@@ -85,8 +115,13 @@ class JSB2_GUI(QMainWindow):
             # Add all top level keys
             self.ui.treeJSB.addTopLevelItems(items)
 
+            self.ui.textConsole.appendPlainText(u'Loaded project "%s" from %s'\
+                 % (jsb2['projectName'], file_name))
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main_window = JSB2_GUI()
     main_window.show()
+    #TODO: Test only
+    main_window.on_action_Open_triggered('/home/churi/python/jsb2-gui/ext.jsb2')
     sys.exit(app.exec_())
